c968cf3743cbd749d474bc42c4efba9340252618
Jerome Fellus <jfellus@gmail.com>
2015-08-26 22:23:11 +0200

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -383,7 +383,7 @@ void compute_errors() {
 
 	DBG("J1=" << J1);
 	DBG("dump");
-	for(int i=0; i<5; i++) node[0].dump_codebook();
+	for(int i=0; i<5; i++) node[i].dump_codebook();
 	dump_consensus();
 	DBG("ok");
 }
